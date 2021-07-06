fc00e64bacbbd1868e9807c268b8f3ecbedc7607
jordicunillerarivera <87013279+jordicunillerarivera@users.noreply.github.com>
2021-07-06 12:31:55 +0200

    Initial commit

---
+++ ComentarioVariasLineas.java
@@ -0,0 +1,18 @@
+
+
+public class ComentarioVariasLineas {
+
+	public static void main(String args[]) {
+		
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Fin de programa");*/
+		for (int i=0; i<6; /* Ahora viene el paso */ i++) {
+			System.out.println(i + " ");
+		}
+
+		System.out.println("Fin del programa");
+
+	}
+
+}
---
+++ test.java
@@ -0,0 +1,27 @@
+
+
+public class test {
+
+public static void main(String args[]) {
+		
+		// Creacion de un objeto integer usando el segundo constructor
+		Integer num1 = new Integer("125");
+
+		// Creacion de un objeto integer usando el primer constructor
+		Integer num2 = new Integer(20);
+
+		// Obtencion del entero que almacena cada objeto Integer.
+		// Si no se hace esto, las lneas siguientes causarian error de compilacion
+		int n1 = num1.intValue();
+		int n2 = num2.intValue();
+		System.out.println("Suma de " + n1 + " y " + n2 + " vale " + (n1+n2));
+
+		if (n1+n2 > 130) {
+			n1++;
+		} else {
+			n1--;
+		}
+
+		System.out.println(n1);
+	}
+}
